8534fe6c0dc95f9331e540b1e821f43b0521453a
Emmanuel <emmanuel.alxs@gmail.com>
2019-01-17 23:11:25 -0300

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,22 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer.'
+}
+
+const completado = {
+    alias: 'c',
+    default: 'true',
+    desc: 'Marca como completado o pendiente la tarea.'
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', { descripcion })
+    .command('actualizar', 'Actualizar una tarea por hacer', { descripcion, completado })
+    .command('listar', 'Mostrar todas las tareas por hacer')
+    .command('borrar', 'Elimina una tarea completada', { descripcion })
+    .help().argv;
+
+module.exports = {
+    argv
+}
